a455c6df837bc1119d128ad98bb960941fb1fab6
shashank1605 <112274522+shashank1605@users.noreply.github.com>
2024-06-17 18:34:04 +0530

    Create logical operators.sql

---
+++ SQL/Operators/logical operators.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE logical_op
+USE logical_op;
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    price DECIMAL(15, 2),
+    created_at DATE,
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    stock INT,
+    location VARCHAR(100)
+);
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    age INT,
+    email VARCHAR(100),
+    city VARCHAR(100)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_amount DECIMAL(15, 2),
+    order_date DATE,
+    payment_mode VARCHAR(50),
+    status VARCHAR(50),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+INSERT INTO products (product_id, product_name, price, created_at, stock, location) VALUES
+(1, 'apple', 150000.00, '2024-01-15', 50, 'Mumbai'),
+(2, 'samsung', 90000.00, '2024-02-20', 30, 'Delhi'),
+(3, 'MI', 20000.00, '2024-03-10', 20, 'Mumbai'),
+(4, 'realme', 12000.00, '2024-04-05', 15, 'Mumbai'),
+(5, 'oneplus', 50000.00, '2024-05-15', 5, 'Chennai');
+
+INSERT INTO customers (customer_id, customer_name, age, email, city) VALUES
+(1, 'Arjun', 28, 'arjun@example.com', 'Mumbai'),
+(2, 'Ajay', 30, 'ajay@example.com', 'Delhi'),
+(3, 'Adithya', 35, 'adithya@example.com', 'Mumbai'),
+(4, 'Shravanth', 40, 'cv@example.com', 'Bangalore'),
+(5, 'Rocky', 30, 'rocky@example.com', 'Mumbai');
+
+INSERT INTO orders (order_id, customer_id, order_amount, order_date, payment_mode, status) VALUES
+(1, 1, 15000.00, '2024-01-20', 'credit_card', 'completed'),
+(2, 2, 9000.00, '2024-02-25', 'upi', 'pending'),
+(3, 3, 5000.00, '2024-03-15', 'net_banking', 'completed'),
+(4, 4, 22000.00, '2024-04-10', 'debit_card', 'completed'),
+(5, 5, 10000.00, '2024-05-20', 'upi', 'completed');
+
+SELECT * FROM products
+WHERE location = 'Mumbai' AND stock > 10;
+
+SELECT * FROM customers
+WHERE city = 'Mumbai'
+OR customer_id IN (SELECT customer_id FROM orders WHERE order_amount > 20000);
+
+SELECT * FROM orders
+WHERE payment_mode <> 'upi' AND status = 'completed';
