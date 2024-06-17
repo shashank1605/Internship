d2a24cd2ffaded0729511364092d02e4737f8ff8
shashank1605 <112274522+shashank1605@users.noreply.github.com>
2024-06-17 18:09:55 +0530

    Update bitwise operator.sql

--- SQL/Operators/bitwise operator.sql
+++ SQL/Operators/bitwise operator.sql
@@ -1,1 +1,28 @@
+CREATE DATABASE bit_wise
+USE  bit_wise
+
+CREATE TABLE binary_numbers (
+    id INT PRIMARY KEY,
+    binary_value1 INT,
+    binary_value2 INT
+);
+
+INSERT INTO binary_numbers (id, binary_value1, binary_value2) VALUES
+(1, 5, 3),
+(2, 12, 10);
+SELECT binary_value1, binary_value2,
+       binary_value1 & binary_value2 AS bitwise_and_result
+FROM binary_numbers
+WHERE id = 1;
+
+SELECT binary_value1, binary_value2,
+       binary_value1 | binary_value2 AS bitwise_or_result
+FROM binary_numbers
+WHERE id = 1;
+
+SELECT binary_value1, binary_value2,
+       binary_value1 ^ binary_value2 AS bitwise_xor_result
+FROM binary_numbers
+WHERE id = 1;
+
 
